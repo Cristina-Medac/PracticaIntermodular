bd3baa6c09434f0be4e62c956305fb3e4f5d5316
Cristina <cam0028@alu.medac.es>
2025-05-21 14:38:51 +0200

    creación del sql
    sql inicial, aun conectado a local.

---
+++ BBDD/script inicial.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS ProyectoFinal;
+CREATE DATABASE ProyectoFinal;
+USE ProyectoFinal;
+
+CREATE TABLE Continentes (
+	nombre VARCHAR(20),
+    superficie INT,
+    clima VARCHAR(50),
+    num_paises INT
+);
