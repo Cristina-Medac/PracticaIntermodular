52996893de31eafd162a5d5705db8a8527f2e7db
Cristina <cam0028@alu.medac.es>
2025-05-22 14:35:44 +0200

    actualización tablas
    Aun no se ha ejecutado el script

--- BBDD/script inicial.sql
+++ BBDD/script inicial.sql
@@ -3,8 +3,30 @@ CREATE DATABASE ProyectoFinal;
 USE ProyectoFinal;
 
 CREATE TABLE Continentes (
-	nombre VARCHAR(20),
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	nombre VARCHAR(20), -- not null
     superficie INT,
     clima VARCHAR(50),
-    num_paises INT
+    num_paises INT -- not null
 );
+
+CREATE TABLE Pais (
+	id_continente VARCHAR(20), -- 
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50),--
+    poblacion  INT,
+    idioma_oficial VARCHAR(50),
+    FOREIGN KEY (continente) REFERENCES Continentes (nombre)
+);
+
+CREATE TABLE Curiosidades (
+	id_pais INT,--
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    gastronomia VARCHAR(50),--
+    curi_historica VARCHAR(255),--
+    dia_nac VARCHAR(50),
+    FOREIGN KEY (nombre_pais) REFERENCES Pais (nombre)
+);
+
+
+
